5f6e869c2e1d74ee50de29af7bcf12c5175d7f92
Guillermo <guillewu@gmail.com>
2024-10-17 09:12:17 +0200

    Make family param optional

--- app/services/product_service.ts
+++ app/services/product_service.ts
@@ -1,14 +1,19 @@
 import Product from '#models/product'
 
 export class ProductService {
-  public static async getProductsWithVariantsAndConditions({ family }: { family: string }) {
-    return await Product.query()
-      .where('family', family)
+  public static async getProductsWithVariantsAndConditions({ family }: { family?: string }) {
+    const query = Product.query()
       .preload('productVariants')
       .preload('conditions', (conditionsQuery) => {
         conditionsQuery.preload('products', (productsQuery) => {
           productsQuery.select('id')
         })
       })
+
+    if (family) {
+      query.where('family', family)
+    }
+
+    return query
   }
 }
