301ab3ffe4a59bd75f52d2432c08c9bee1270114
Guillermo <guillewu@gmail.com>
2024-11-11 11:14:26 +0100

    Use serializer to reduce response size

--- backend/app/services/product_service.ts
+++ backend/app/services/product_service.ts
@@ -14,8 +14,41 @@ export class ProductService {
       query.where('family', family)
     }
     const results = await query
-    return results.reduce((acc: any, curr) => {
-      acc[curr.id] = curr
+
+    const resultsJson = results.map((res) =>
+      res.serialize({
+        fields: {
+          pick: ['id', 'name', 'family'],
+        },
+        relations: {
+          productVariants: {
+            fields: ['id', 'name', 'imageUrl', 'price', 'productId'],
+          },
+          conditions: {
+            fields: ['rule'],
+            relations: {
+              products: {
+                fields: ['id'],
+              },
+            },
+          },
+        },
+      })
+    )
+
+    return resultsJson.reduce((acc: any, product) => {
+      const requiredProductIds: any[] = []
+
+      product.conditions.forEach((condition: { products: any[] }) => {
+        condition.products.forEach((prod) => {
+          if (prod.id !== product.id) {
+            requiredProductIds.push(prod.id)
+          }
+        })
+      })
+      delete product.conditions
+      product.requiredProductIds = requiredProductIds
+      acc[product.id] = product
       return acc
     }, {})
   }
